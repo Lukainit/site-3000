e3db874eb9476e4b4010f9f74f33488189f81a78
hpxxph <santanazar123@icloud.com>
2022-11-24 15:19:16 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+﻿let chooseBtn = document.getElementById('choose'),
+    receiveBtn = document.getElementById('receive'),
+    heading = document.getElementsByTagName('h2')[0],
+    nameInput = document.getElementsByClassName('contactform_name')[0],
+    phoneInput = document.querySelector('.contactform_phone'),
+    mailInput = document.querySelectorAll('.contactform_mail')[0],
+    modal = document.querySelector('.modal'),
+    closeBtn = document.querySelector('.close'),
+    text = document.getElementsByName('message')[0];
+
+function hover () {
+
+    heading.textContent = "действительно все!"
+};
+
+chooseBtn.addEventListener('mouseenter', function(){
+    heading.textContent = "действительно все!"
+});
+
+
+receiveBtn.addEventListener('click', function(){
+    modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function(){
+    modal.style.display = 'block';
+});
+
+nameInput.addEventListener('input', function(){
+    text.value = `меня зовут ${nameInput.value} и я хочу спросить: `
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
